25ef9f23f7f2cd7d6400061616fac25af0713b82
unknown <sifatniloy18@gmail.com>
2022-10-14 12:40:13 -0600

    new user added from input

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const app = express();
+
+app.use(cors())
+app.use(express.json())
 const port = 5000;
 
 app.get('/', (req, res) => {
@@ -11,7 +15,7 @@ const users = [
     { id: 2, name: 'mousumi', email: 'mousumi@gmail.com', phone: '014463450' },
     { id: 3, name: 'suchorita', email: 'shuchorita@gmail.com', phone: '01454600' },
     { id: 4, name: 'sonya', email: 'sonya@gmail.com', phone: '014345560' },
-    { id: 5, name: 'susmita', email: 'susmita@gmail.com', phone: '01443453400' },
+    { id: 5, name: 'susmita', email: 'susmita@gmail.com', phone: '01443453400' }
 ]
 app.get('/users', (req, res) => {
     const search = req.query.search;
@@ -23,6 +27,16 @@ app.get('/users', (req, res) => {
         res.send(users)
     }
 
+})
+
+
+app.post('/users', (req, res) => {
+    const newUser = req.body;
+    newUser.id = users.length;
+    users.push(newUser);
+    console.log('hitting the post', req.body);
+    // res.send(JSON.stringify(newUser))
+    res.json(newUser)
 })
 app.get('/users/:id', (req, res) => {
     const id = req.params.id;
@@ -38,3 +52,6 @@ app.get('/fruits/mangoes/fazli', (req, res) => {
 app.listen(port, () => {
     console.log('listening to port', port)
 })
+
+
+
